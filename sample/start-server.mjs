6187ea586ec8b72b6dd333cd61d9f7a2a9459a5c
Eyeball Edward <edwardyeung39@gmail.com>
2025-11-23 15:38:50 +0800

    return alert text

--- sample/start-server.mjs
+++ sample/start-server.mjs
@@ -17,9 +17,9 @@ server.on('non_specific', message => {
   console.log('Got non specific V/Line message! Contents:', message)
 })
 
-server.onMessage({
+console.log(server.onMessage({
   subject: '',
-  html: 'The 17:15 Southern Cross - Bacchus Marsh service will run at a reduced capacity of 3 VLocity carriages.'
-})
+  html: 'The 17:15 Southern Cross - Bacchus Marsh service will run at a reduced capacity of 3 VLocity carriages.\n More information at vline.com.au   VLine & PTV Logo   You are receiving this email because your email address is registered at vline.com.au/inform.  To unsubscribe or update your contact/journey details, visit vline.com.au/login.  For general travel information please call 1800 800 007 or visit vline.com.au.  For weekly updates about planned service changes across the public transport network, subscribe to PTV\'s weekly travel update.'
+}))
 
 server.setup()
